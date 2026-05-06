af4d64ff61bf585fb17f1fe6d02d6ca3451f4db2
almightyzeus <chinmaypgoa@gmail.com>
2026-05-06 10:22:07 +0530

    Delete files generated due to build.

--- backend/src/services/googleClient.js
+++
@@ -1,3 +0,0 @@
-import { google } from 'googleapis';
-import { env } from '../config/env.js';
-export const oauth2Client = new google.auth.OAuth2(env.google.clientId, env.google.clientSecret, env.google.redirectUri);
